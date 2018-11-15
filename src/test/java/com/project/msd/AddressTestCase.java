7c3e04aff82c711e23d9a21aa40e55f0bbeb15cc
Solonaru Viorel <viorelsolonaru96@gmail.com>
2018-11-15 22:43:11 +0200

    Added unit tests for county and city classes

---
+++ src/test/java/com/project/msd/AddressTestCase.java
@@ -0,0 +1,38 @@
+package com.project.msd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.project.msd.location.Address;
+
+public class AddressTestCase {
+	
+	Address address;
+
+	@Before
+	public void setUp() throws Exception {
+		address = new Address();
+	}
+
+	@Test
+	public void object_should_be_not_null() {		
+		assertNotEquals(address, null);
+	}
+	
+	@Test
+	public void getId_should_return_null_if_not_set() {
+		assertEquals(address.getId(), null);
+	}
+	
+	@Test
+	public void on_setId_getId_should_return_id() {
+		Integer id = 1;
+		
+		address.setId(id);
+		
+		assertEquals(address.getId(), id);
+	}
+}
---
+++ src/test/java/com/project/msd/CityTestCase.java
@@ -0,0 +1,67 @@
+package com.project.msd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.project.msd.location.City;
+import com.project.msd.location.County;
+
+public class CityTestCase {
+	
+	City city;
+
+	@Before
+	public void setUp() throws Exception {
+		city = new City();
+	}
+
+	@Test
+	public void object_should_be_not_null() {		
+		assertNotEquals(city, null);
+	}
+	
+	@Test
+	public void getId_should_return_null_if_not_set() {
+		assertEquals(city.getId(), null);
+	}
+	
+	@Test
+	public void on_setId_getId_should_return_id() {
+		Integer id = 1;
+		
+		city.setId(id);
+		
+		assertEquals(city.getId(), id);
+	}
+	
+	@Test
+	public void getName_should_return_null_if_not_set() {
+		assertEquals(city.getName(), null);
+	}
+	
+	@Test
+	public void on_setName_getName_should_return_name() {
+		String name = "Iasi";
+		
+		city.setName(name);
+		
+		assertEquals(city.getName(), name);
+	}
+	
+	@Test
+	public void getCounty_should_return_null_if_not_set() {
+		assertEquals(city.getCounty(), null);
+	}
+	
+	@Test
+	public void on_setCounty_getCounty_should_return_county() {
+		County county = new County("Iasi", "Iasi");
+		
+		city.setCounty(county);
+		
+		assertEquals(city.getCounty(), county);
+	}
+
+}
