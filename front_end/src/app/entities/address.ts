8daa0ff6b4e09fcccb07a88c28503388a1f4f88f
claudiaflorea <claudiaflorea0@gmail.com>
2018-12-08 17:23:29 +0200

    Added list of employees for back-office

---
+++ front_end/src/app/entities/address.ts
@@ -0,0 +1,5 @@
+export class Address {
+    id: Number;
+    street: String ;
+	nr: Number ;
+}
--- front_end/src/app/entities/employee.ts
+++ front_end/src/app/entities/employee.ts
@@ -1,7 +1,11 @@
+import { Address } from "./address";
+
 export class Employee {
     id: Number;
     username: String;
     password: String;
     name: String;
     email: String;
+    creationDate: Date;
+    address: Address;
 }
