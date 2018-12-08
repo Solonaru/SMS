ee71c9a59717871fa90cf378ff5b059bd462053e
claudiaflorea <claudiaflorea0@gmail.com>
2018-12-08 18:57:14 +0200

    Enabled Cross Origin Requests for a RESTful Web Service

--- src/main/java/com/project/sms/entities/subscription/SubscriptionController.java
+++ src/main/java/com/project/sms/entities/subscription/SubscriptionController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.project.sms.utils.DisplayData;
 
 @RestController
 @RequestMapping("/subscription")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SubscriptionController {
 
 	@Autowired
