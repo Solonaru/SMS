8f633b73ec9019a2890d13b83c92ee9b82a1031d
Solonaru Viorel <viorelsolonaru96@gmail.com>
2018-12-15 11:04:32 +0200

    Solved issue with empty items list for parent categories

--- src/main/java/com/project/sms/entities/item/ItemController.java
+++ src/main/java/com/project/sms/entities/item/ItemController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.sms.entities.category.ICategoryService;
 import com.project.sms.utils.DisplayData;
 
 @RestController
@@ -22,63 +23,82 @@ public class ItemController {
 
 	@Autowired
 	private IItemService itemService;
-	
+
+	@Autowired
+	private ICategoryService categoryService;
+
 	@Autowired
 	private DisplayData dataDisplay;
 
 	@RequestMapping(value = "/{itemId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Optional<Item> findItemById(@PathVariable("itemId") int itemId) {
-		dataDisplay.printCrudInfo(itemId); 
+		dataDisplay.printCrudInfo(itemId);
 		return itemService.findItemById(itemId);
 	}
 
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Item> getItems() {
-		dataDisplay.printCrudInfo(); 
+		dataDisplay.printCrudInfo();
 		return itemService.findAllItems();
 	}
-	
+
 	@RequestMapping(value = "/all/listed", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Item> getListedItems() {
 		dataDisplay.printCrudInfo();
 		List<Item> listedItems = new ArrayList<Item>();
-		for (Item item: itemService.findAllItems()) {
+		for (Item item : itemService.findAllItems()) {
 			if (item.isListed()) {
 				listedItems.add(item);
 			}
 		}
-		
+
 		return listedItems;
 	}
-	
+
 	@RequestMapping(value = "/all/listed/{categoryId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Item> getListedItemsByCategoryId(@PathVariable("categoryId") int categoryId) {
 		dataDisplay.printCrudInfo();
 		List<Item> listedItems = new ArrayList<Item>();
-		for (Item item: itemService.findAllItems()) {
-			if (item.isListed() && item.getCategory().getId().equals(categoryId)) {
+		for (Item item : itemService.findAllItems()) {
+			if (this.checkItem(item, categoryId)) {
 				listedItems.add(item);
 			}
 		}
-		
+
 		return listedItems;
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void insertItem(@RequestBody Item item) {
-		dataDisplay.printCrudInfo(); 
+		dataDisplay.printCrudInfo();
 		itemService.insertItem(item);
 	}
 
 	@RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void updateItem(@RequestBody Item item) {
-		dataDisplay.printCrudInfo(); 
+		dataDisplay.printCrudInfo();
 		itemService.updateItem(item);
 	}
 
 	@RequestMapping(value = "/delete/{itemId}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void deleteItem(@PathVariable("itemId") int itemId) {
-		dataDisplay.printCrudInfo(itemId); 
+		dataDisplay.printCrudInfo(itemId);
 		itemService.deleteItemById(itemId);
+	}
+
+	// ----- Helper methods -----
+	private boolean checkItem(Item item, int categoryId) {
+		boolean ret_val = false;
+
+		if (item.isListed()) {
+			if (item.getCategory().getId().equals(categoryId)) {
+				ret_val = true;
+			} else if (categoryService.findCategoryById(categoryId).get().getChildCategories()
+					.indexOf(item.getCategory()) != -1) {
+				ret_val = true;
+			}
+		}
+
+		return ret_val;
 	}
 }
