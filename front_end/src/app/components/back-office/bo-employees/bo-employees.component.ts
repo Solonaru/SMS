03977a1dccc9e7fc75b57170be75f18bf1adf0b0
Solonaru Viorel <viorelsolonaru96@gmail.com>
2018-12-21 20:32:55 +0200

    Revert "Added removeEmplyoee and removeItem for the back office"
    This reverts commit ad13c29b2770b653c9f00450acb06f3082a7aaa3.

--- front_end/src/app/components/back-office/bo-employees/bo-employees.component.ts
+++ front_end/src/app/components/back-office/bo-employees/bo-employees.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../../providers/services/employee.service';
 import { Employee } from '../../../entities/employee';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-bo-employees',
@@ -14,9 +10,8 @@ import { map } from 'rxjs/operators';
 export class BoEmployeesComponent implements OnInit {
 
   employees: Employee[];
-  private BASE_URL = 'http://localhost:8090/employee';
 
-  constructor(private employeeService: EmployeeService, private http: HttpClient) { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.populateItems();
@@ -26,14 +21,4 @@ export class BoEmployeesComponent implements OnInit {
     this.employeeService.getEmployees().subscribe(data => { this.employees = data; console.log(this.employees); });
   }
 
-  removeEmployee ( employee:Employee): Observable<any> {
-    return this.http.delete(this.BASE_URL + 'delete/' + employee.id)
-    .pipe(
-      map(
-      (response:any) => {return response}
-        )
-        );
-  }
 }
-
-
