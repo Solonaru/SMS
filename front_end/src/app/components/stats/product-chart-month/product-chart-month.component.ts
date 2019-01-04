909049cf0bd3d795d0904207925c5bdc24a7c035
Solonaru Viorel <viorelsolonaru96@gmail.com>
2019-01-04 21:31:16 +0200

    Added a new features to the product chart

--- front_end/src/app/components/stats/product-chart-month/product-chart-month.component.ts
+++ front_end/src/app/components/stats/product-chart-month/product-chart-month.component.ts
@@ -5,6 +5,8 @@ import { ItemService } from '../../../providers/services/item.service';
 import { Item } from '../../../entities/item';
 import { Router } from '@angular/router';
 import { ProductStatsService } from '../../../providers/services/productstats.service';
+import { Category } from '../../../entities/category';
+import { CategoryService } from '../../../providers/services/category.service';
 
 @Component({
   selector: 'app-product-chart-month',
@@ -13,63 +15,71 @@ import { ProductStatsService } from '../../../providers/services/productstats.se
 })
 export class ProductChartMonthComponent implements OnInit {
 
-  items: Item[];
+  categories: Category[];
   productsStatisticDataMonth: Map<Number, Number>;
 
-  private displayAverage: boolean = true;
+  items1: Item[];
+  item1: Item;
+  private selectedValue1;
+  private selectedCategory1;
 
-  private averageValue = 0;
-  private averageLabel = "";
-  private dataPoints = [];
+  private displayAverage1: boolean = true;
 
-  item: Item;
-  private selectedValue;
+  private averageValue1 = 0;
+  private averageLabel1 = "";
+  private dataPoints1 = [];
 
-  constructor(private itemService: ItemService, private productStatsService: ProductStatsService, private router: Router) { }
+
+  items2: Item[];
+  item2: Item;
+  private selectedValue2;
+  private selectedCategory2;
+
+  private displayAverage2: boolean = true;
+
+  private averageValue2 = 0;
+  private averageLabel2 = "";
+  private dataPoints2 = [];
+
+  constructor(private categoryService: CategoryService, private itemService: ItemService, private productStatsService: ProductStatsService, private router: Router) { }
 
   ngOnInit() {
-    this.populateItems();
+    this.populateCategories();
+    // this.populateItems();
   }
 
-  populateItems() {
-    this.itemService.getItems().subscribe(data => { this.items = data; });
+  populateCategories() {
+    this.categoryService.getCategories().subscribe(data => {this.categories = data});
   }
 
-  populateChart(productId: Number) {
-    this.productStatsService.getProductsStatisticDataMonth(productId).subscribe(data => {
-      this.productsStatisticDataMonth = data;
+  // populateItems() {
+  //   this.itemService.getItems().subscribe(data => { this.items = data; });
+  // }
 
-      this.productStatsService.getAverageFromStatisticData(this.productsStatisticDataMonth).subscribe(data => {
-        console.log("Display average? " + this.displayAverage);
+  populateChart(productId: Number, displayAverage, num, dataPoints) {
+    this.productStatsService.getProductsStatisticDataMonth(productId).subscribe(statData => {
+      this.productStatsService.getAverageFromStatisticData(statData).subscribe(average => {
 
-        if (this.displayAverage) {
-          this.averageValue = data;
-          this.averageLabel = "Average = " + this.averageValue;
+        if (displayAverage) {
+          switch (num) {
+            case 1:
+              this.averageValue1 = average;
+              this.averageLabel1 = "Average = " + this.averageValue1;
+              break;
+            case 2:
+              this.averageValue2 = average;
+              this.averageLabel2 = "Average = " + this.averageValue2;
+              break;
+          }
         }
 
+        this.generateData(dataPoints, statData);
         this.generateChart();
       });
     });
   }
 
   generateChart() {
-    let y = 0;
-    let x = 0;
-    let month = 0;
-    let year = 0;
-
-    for (var key in this.productsStatisticDataMonth) {
-      if (this.productsStatisticDataMonth.hasOwnProperty(key)) {
-        console.log(key + " -> " + this.productsStatisticDataMonth[key]);
-        y = <any>this.productsStatisticDataMonth[key];
-        x = <any>key;
-        month = x % 100;
-        year = Math.floor(x / 100);
-
-        this.dataPoints.push({ x: new Date(year, month, 1), y: y });
-      }
-    };
-
     let chart = new CanvasJS.Chart("chartContainer", {
       zoomEnabled: true,
       animationEnabled: true,
@@ -78,42 +88,118 @@ export class ProductChartMonthComponent implements OnInit {
         text: "Product Sales by Month"
       },
       subtitles: [{
-        text: "Try Zooming and Panning"
+        // text: "Try Zooming and Panning"
       }],
       axisX: {
+        title: "Month",
         interval: 1,
         intervalType: "month",
         valueFormatString: "MMM"
       },
       axisY: {
+        title: "Total sales",
         stripLines: [{
-          value: this.averageValue,
-          label: this.averageLabel
-        }]
+          value: this.averageValue1,
+          label: this.averageLabel1,
+          color: "#0059b3",
+          labelFontColor: "#0059b3"
+        },
+        {
+          value: this.averageValue2,
+          label: this.averageLabel2,
+          color: "#ff0200",
+          labelFontColor: "#ff0200"
+        }
+        ]
       },
+      legend: {
+        cursor: "pointer",
+        verticalAlign: "top",
+        horizontalAlign: "center",
+        // dockInsidePlotArea: true
+      },
+      // toolTip: {
+      //   shared: true
+      // },
       data: [
         {
+          name: "Product 1",
           type: "spline",
           xValueFormatString: "MMM, YYYY",
-          dataPoints: this.dataPoints
-        }]
+          color: "#003366",
+          showInLegend: true,
+          dataPoints: this.dataPoints1
+        },
+        {
+          name: "Product 2",
+          type: "spline",
+          xValueFormatString: "MMM, YYYY",
+          color: "#b30100",
+          showInLegend: true,
+          dataPoints: this.dataPoints2
+        }
+      ]
     });
 
     chart.render();
-    this.averageValue = 0;
-    this.averageLabel = "";
-    this.dataPoints = [];
   }
 
-  changeItem() {
-    this.itemService.getItemById(this.selectedValue).subscribe(data => {
-      this.item = data;
-      this.populateChart(this.item.id);
+  generateData(dataPoints, statData: Map<Number, Number>) {
+    let y = 0;
+    let x = 0;
+    let month = 0;
+    let year = 0;
+
+    for (var key in statData) {
+      if (statData.hasOwnProperty(key)) {
+        console.log(key + " -> " + statData[key]);
+        y = <any>statData[key];
+        x = <any>key;
+        month = x % 100;
+        year = Math.floor(x / 100);
+
+        dataPoints.push({ x: new Date(year, month, 1), y: y });
+      }
+    };
+  }
+
+  changeItem1() {
+    this.averageValue1 = 0;
+    this.averageLabel1 = "";
+    this.dataPoints1 = [];
+
+    this.itemService.getItemById(this.selectedValue1).subscribe(data => {
+      this.item1 = data;
+      this.populateChart(this.item1.id, this.displayAverage1, 1, this.dataPoints1);
     });
   }
 
-  onDisplayAverage() {
-    this.changeItem();
+  onDisplayAverage1() {
+    this.changeItem1();
+  }
+
+  changeItem2() {
+    this.averageValue2 = 0;
+    this.averageLabel2 = "";
+    this.dataPoints2 = [];
+
+    this.itemService.getItemById(this.selectedValue2).subscribe(data => {
+      this.item2 = data;
+      this.populateChart(this.item2.id, this.displayAverage2, 2, this.dataPoints2);
+    });
+  }
+
+  onDisplayAverage2() {
+    this.changeItem2();
+  }
+
+
+  changeCategory1() {
+    this.itemService.getListedItemsByCategoryId(this.selectedCategory1).subscribe(data => {this.items1 = data});
+  }
+
+  changeCategory2() {
+    this.itemService.getListedItemsByCategoryId(this.selectedCategory2).subscribe(data => {this.items2 = data});
   }
 
 }
